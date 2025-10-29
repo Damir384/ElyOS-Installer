b79f5fece2204fa80929848dc8bc5dac1b109374
Damir384 <adamir564@gmail.com>
2025-10-30 02:07:37 +0700

    fix function dialog_text

--- src/dialogs.cpp
+++ src/dialogs.cpp
@@ -5,26 +5,38 @@ namespace dialogs {
         box(win, 0, 0);
         mvwprintw(win, 0, 1, "%s", title.c_str());
         wrefresh(win);
-        int h, w;
+        int h, w = 0;
         getmaxyx(win, h, w);
 
-        int text_lines = content.size() / w;
+        int pad_h = 1;
 
-        WINDOW* pad = newpad(text_lines + 1, w - 2);
+        for (size_t i = 0; i < content.size(); i++){
+            if (content[i] == '\n')
+                pad_h++;
+        }
+
+        int text_lines = ((content.size() - pad_h) / (w - 2)) + pad_h;
+
+        int max = (text_lines - h + 1);
+
+        if (max < -1)
+            max = -1;
+
+        WINDOW* pad = newpad(text_lines, w - 2);
 
         mvwprintw(pad, 0, 0, "%s", content.c_str());
 
         int scroll = 0;
 
         while (true) {
-            prefresh(pad, scroll, 0, 2, 3, h - 1, w - 1);
-
+            prefresh(pad, scroll, 0, 2, 3, h-1, w);
             int key = wgetch(win);
             if (key == KEY_UP && scroll >= 1)
                 scroll--;
-            else if (key == KEY_DOWN && scroll <= text_lines){
+            else if (key == KEY_DOWN && scroll <= max)
                 scroll++;
-            }
+            else if (key == KEY_HOME && scroll)
+                scroll = 0;
             else if (key == 10 || key == 27)
                 break;
         }
