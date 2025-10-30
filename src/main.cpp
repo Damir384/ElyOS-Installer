ea2230dee70a6466780913fa288ab303540a84c4
Damir384 <adamir564@gmail.com>
2025-10-30 20:17:57 +0700

    Add debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <chrono>
 #include "dialogs.hpp"
 
+/**
+ * функция вывода логотипа в консоль
+ */
 void start_msg() {
     std::cout << "┌────────────────────┐" << "\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(200)); // задержка 200 мс
@@ -21,8 +24,20 @@ void start_msg() {
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 }
 
-int main() {
-    start_msg(); //функция вывода логотипа системы в консоль
+int main(int argc, char* argv[]) {
+
+    bool debug = false;
+
+    if (argc > 1){
+        for (int i = 0; i<argc; i++){
+            if (std::string(argv[i]) != "--debug")
+                debug = true;
+        }
+    }
+
+    // Проверка режима работы программы
+    if (!debug)
+        start_msg();
 
     setlocale(LC_ALL, "");  // поддержка всех локалей системы
     initscr(); // инициализация ncurses
