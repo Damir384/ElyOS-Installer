53c6a6a30710c4e0d4d0bb556417c691be7d7173
Damir384 <adamir564@gmail.com>
2025-10-31 01:05:18 +0700

    Edit debug mode and add start session function

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "dialogs.hpp"
 
 /**
- * функция вывода логотипа в консоль
+ * Функция вывода логотипа в консоль
  */
 void start_msg() {
     std::cout << "┌────────────────────┐" << "\n";
@@ -24,6 +24,32 @@ void start_msg() {
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 }
 
+/**
+ * Функция получения размера окна по текущему разрешению консоли
+ */
+bool get_resolution(int & height, int & width) {
+    if (COLS < 100 || LINES < 37) {
+        return false;
+    } else {
+        height = 35;
+        width = 96;
+        return true;
+    }
+}
+
+/**
+ * Стартует сессию ncurses
+ */
+void session_start() {
+    setlocale(LC_ALL, "");  // поддержка всех локалей системы
+    initscr(); // инициализация ncurses
+    start_color(); // включение цветов
+    cbreak(); // отключаем полный контроль над клавиатурой
+    noecho(); // отключение вывода вводимых символов
+    keypad(stdscr, TRUE); // включение полного захвата клавиатуры
+    curs_set(0); // видимость курсора от 0 до 2
+}
+
 int main(int argc, char* argv[]) {
 
     bool debug = false;
@@ -35,20 +61,24 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    int height = 0;
+    int width = 0;
+
     // Проверка режима работы программы
-    if (!debug)
+    if (!debug){
         start_msg();
+        session_start();
+        if(!get_resolution(height, width)){
+            endwin();
+            std::cout << "Minimal resolution is 100x37" << std::endl;
+            return 1;
+        }
+    }else {
+        session_start();
+        height = LINES - 2;
+        width = COLS - 4;
+    }
 
-    setlocale(LC_ALL, "");  // поддержка всех локалей системы
-    initscr(); // инициализация ncurses
-    start_color(); // включение цветов
-    cbreak(); // отключаем полный контроль над клавиатурой
-    noecho(); // отключение вывода вводимых символов
-    keypad(stdscr, TRUE); // включение полного захвата клавиатуры
-    curs_set(0); // видимость курсора от 0 до 2
-
-    int height = LINES - 2;
-    int width = COLS - 4;
     WINDOW* win = newwin(height, width, 1, 2);
     keypad(win, TRUE);
     std::string title = "Test";
