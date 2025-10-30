15acec809da83210da2a91e7d2c4462354292c28
Damir384 <adamir564@gmail.com>
2025-10-30 16:34:48 +0700

    Adding comments and improving pad controls

--- src/dialogs.cpp
+++ src/dialogs.cpp
@@ -1,13 +1,38 @@
 #include "dialogs.hpp"
 
 namespace dialogs {
+    /**
+     * Функция отображения текста внутри окна win
+     * 
+     * @param win
+     * принимает на вход указатель на окно типа WINDOW* и выводит текст внутри этого окна
+     * 
+     * @param title
+     * принимает на вход строку типа string и выводит её как заголовок окра
+     * 
+     * @param content
+     * принимает на вход строку типа string и выводит её как содержимое окна с возможностью прокрутки
+     */
     int dialog_text(WINDOW* win, std::string title, std::string content){
+        // Создаём рамку в окне win
         box(win, 0, 0);
-        mvwprintw(win, 0, 1, "%s", title.c_str());
-        wrefresh(win);
-        int h, w = 0;
-        getmaxyx(win, h, w);
 
+        // Выводим заголовок title в окно win
+        mvwprintw(win, 0, 1, "%s", title.c_str());
+
+        // Выводим изменения
+        wrefresh(win);
+
+        // Расположение окна win
+        int h, w = 0;
+
+        // Размер окна win
+        int wh, ww = 0;
+
+        getmaxyx(win, h, w);
+        getbegyx(win, wh, ww);
+
+        // Общая высота пада
         int pad_h = 1;
 
         for (size_t i = 0; i < content.size(); i++){
@@ -15,8 +40,10 @@ namespace dialogs {
                 pad_h++;
         }
 
+        // Общее количество строк выводимых в паде
         int text_lines = ((content.size() - pad_h) / (w - 2)) + pad_h;
 
+        // Ограничение прокрутки пада 
         int max = (text_lines - h + 1);
 
         if (max < -1)
@@ -26,10 +53,12 @@ namespace dialogs {
 
         mvwprintw(pad, 0, 0, "%s", content.c_str());
 
+        // Скрол пада
         int scroll = 0;
 
+        // Цикл обновления пада и логика управления выводом
         while (true) {
-            prefresh(pad, scroll, 0, 2, 3, h-1, w);
+            prefresh(pad, scroll, 0, wh+1, ww+1, wh+h-1, ww+w);
             int key = wgetch(win);
             if (key == KEY_UP && scroll >= 1)
                 scroll--;
@@ -37,8 +66,10 @@ namespace dialogs {
                 scroll++;
             else if (key == KEY_HOME && scroll)
                 scroll = 0;
-            else if (key == 10 || key == 27)
-                break;
+            else if (key == 10 || key == 27){
+                delwin(pad);
+                return 0;
+            }
         }
 
         delwin(pad);
