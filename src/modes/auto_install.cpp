39378a7d5b742682833c596c26ef438154c219c2
Damir384 <adamir564@gmail.com>
2025-11-24 01:22:49 +0700

    Minor changes

--- src/modes/auto_install.cpp
+++ src/modes/auto_install.cpp
@@ -3,8 +3,11 @@
 int AutoInstall::run(WINDOW* win) {
         StepManager manager;
 
+        manager.selectAddPackages = "selectAddPackages";
+
         manager.add(std::make_unique<ShowPartitionInfo>());
         manager.add(std::make_unique<ShowDisks>());
+        manager.add(std::make_unique<ShowPartitions>());
 
         manager.run(win);
         return 0;
