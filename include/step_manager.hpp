b3c08480cbf03eef655f5ef39ec8bd52698e6780
Damir384 <adamir564@gmail.com>
2025-11-24 01:22:08 +0700

    Minor changes

--- include/step_manager.hpp
+++ include/step_manager.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include "step.hpp"
+#include "context.hpp"
 #include <vector>
 #include <memory>
 #include <ncurses.h>
+#include <algorithm>
 
 class StepManager {
 public:
     void add(std::unique_ptr<Step> step);
 
     StepResult run(WINDOW* win);
+    std::string selectAddPackages;
 
 private:
     std::vector<std::unique_ptr<Step>> steps;
+    int skip();
 };
